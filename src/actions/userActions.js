df14b1e5a0f2ccde402fe3c4647c1d166d22c39f
rishi <119162955+RishithReddyRR@users.noreply.github.com>
2023-09-28 08:49:04 +0530

    Update userActions.js

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -12,7 +12,6 @@ export const login = (email, password) => async (dispatch) => {
       { email, password },
       config
     );
-    document.cookie=`token=${data.token};`
     dispatch({ type: "LOGIN_SUCCESS", payload: data.user });
   } catch (error) {
     dispatch({ type: "LOGIN_FAIL", payload: error.response.data.message });
